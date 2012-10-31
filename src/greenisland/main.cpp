9d2b2f0f92719dbb21319a84cdcaa77f3df233bb
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-10-31 21:51:32 +0100

    Change license to GPL 3 or (at your option) any later version.
    This seems the most appropriate license for an executable.

--- src/greenisland/main.cpp
+++ src/greenisland/main.cpp
@@ -6,11 +6,12 @@
  * Author(s):
  *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
  *
- * $BEGIN_LICENSE:GPL3$
+ * $BEGIN_LICENSE:GPL3+$
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 3.
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
