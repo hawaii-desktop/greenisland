0f858ec375f74fff48f217ff4c8a9dda73ea1386
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-11-25 22:26:15 +0100

    Check whether XDG_RUNTIME_DIR is valid or not.
    XDG_RUNTIME_DIR must point to a directory that exist (usually
    /run/user/UID) and its permission must be 0700.

--- src/greenisland/main.cpp
+++ src/greenisland/main.cpp
@@ -25,6 +25,7 @@
  ***************************************************************************/
 
 #include <QDebug>
+#include <QDir>
 #include <QScreen>
 #include <QStringList>
 #include <QWindow>
@@ -33,8 +34,76 @@
 
 #include "greenisland.h"
 
+#include <unistd.h>
+
+static int convertPermission(const QFileInfo &fileInfo)
+{
+    int p = 0;
+
+    if (fileInfo.permission(QFile::ReadUser))
+        p += 400;
+    if (fileInfo.permission(QFile::WriteUser))
+        p += 200;
+    if (fileInfo.permission(QFile::ExeUser))
+        p += 100;
+    if (fileInfo.permission(QFile::ReadGroup))
+        p += 40;
+    if (fileInfo.permission(QFile::WriteGroup))
+        p += 20;
+    if (fileInfo.permission(QFile::ExeGroup))
+        p += 10;
+    if (fileInfo.permission(QFile::ReadOther))
+        p += 4;
+    if (fileInfo.permission(QFile::WriteOther))
+        p += 2;
+    if (fileInfo.permission(QFile::ExeOther))
+        p += 1;
+
+    return p;
+}
+
+static void verifyXdgRuntimeDir()
+{
+    QByteArray dirName = qgetenv("XDG_RUNTIME_DIR");
+
+    if (dirName.isEmpty()) {
+        QString msg = QObject::tr(
+                          "The XDG_RUNTIME_DIR environment variable is not set.\n"
+                          "Refer to your distribution on how to get it, or read\n"
+                          "http://www.freedesktop.org/wiki/Specifications/basedir-spec\n"
+                          "on how to implement it.\n");
+        qFatal(msg.toUtf8());
+    }
+
+    QFileInfo fileInfo(dirName);
+
+    if (!fileInfo.exists()) {
+        QString msg = QObject::tr(
+                          "The XDG_RUNTIME_DIR environment variable is set to "
+                          "\"%1\", which doesn't exist.\n").arg(dirName.constData());
+        qFatal(msg.toUtf8());
+    }
+
+    if (convertPermission(fileInfo) != 700 || fileInfo.ownerId() != getuid()) {
+        QString msg = QObject::tr(
+                          "XDG_RUNTIME_DIR is set to \"%1\" and is not configured correctly.\n"
+                          "Unix access mode must be 0700, but is 0%2.\n"
+                          "It must also be owned by the current user (UID %3), "
+                          "but is owned by UID %4 (\"%5\").\n")
+                      .arg(dirName.constData())
+                      .arg(convertPermission(fileInfo))
+                      .arg(getuid())
+                      .arg(fileInfo.ownerId())
+                      .arg(fileInfo.owner());
+        qFatal(msg.toUtf8());
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    // Check whether XDG_RUNTIME_DIR is ok or not
+    verifyXdgRuntimeDir();
+
     // Assume the xcb platform if the DISPLAY environment variable is defined,
     // otherwise go for kms
     if (!qgetenv("DISPLAY").isEmpty())
