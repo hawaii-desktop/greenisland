93835c578ae04febe047afd7113da84d816daac1
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-04-23 08:24:15 +0200

    screencast: Command line arguments and QPA check
    Issue: #5

--- src/tools/screencaster/main.cpp
+++ src/tools/screencaster/main.cpp
@@ -28,14 +28,54 @@
  * $END_LICENSE$
  ***************************************************************************/
 
+#include <QtCore/QCommandLineParser>
+#include <QtCore/QCommandLineOption>
+#include <QtCore/QDebug>
 #include <QtGui/QGuiApplication>
 
+#include "config.h"
 #include "screencaster.h"
+
+#define TR(x) QT_TRANSLATE_NOOP("Command line parser", QStringLiteral(x))
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    app.setApplicationName(QStringLiteral("greenisland-screencaster"));
+    app.setApplicationVersion(QStringLiteral(GREENISLAND_VERSION_STRING));
+    app.setQuitOnLastWindowClosed(false);
 
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QStringLiteral("Green Island Screencast recorder"));
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    // Output file name
+    QCommandLineOption outputOption(QStringList() << QStringLiteral("o") << QStringLiteral("output"),
+                               TR("Output file name"), TR("filename"));
+    parser.addOption(outputOption);
+
+    // Number of frames to capture
+    QCommandLineOption framesOption(QStringList() << QStringLiteral("n") << QStringLiteral("number-of-frames"),
+                                    TR("Number of frames to record"), TR("number-of-frames"));
+    parser.addOption(framesOption);
+
+    // Parse command line
+    parser.process(app);
+
+    // Need to be running with wayland QPA
+    if (!QGuiApplication::platformName().startsWith(QStringLiteral("wayland"))) {
+        qCritical() << "greenisland-screencast requires a Wayland session";
+        return 1;
+    }
+
+    // Arguments check
+    if (!parser.isSet(outputOption)) {
+        qCritical() << "Output file name is mandatory!";
+        return 1;
+    }
+
+    // Start the screen caster
     ScreenCaster screenCaster;
     return app.exec();
 }
