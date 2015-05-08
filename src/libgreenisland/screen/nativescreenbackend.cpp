e92f757dee3c9c457d1e568a217d11208f593df7
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-05-08 22:42:11 +0200

    Revert "QtDeclarative bug workaround"
    This reverts commit d0e4d94055439d76f17f1ef886720f03fe017d58.
    
    Nasty QtDeclarative bug been fixed upstream.
    
    For more information please read here:
    
    https://bugreports.qt.io/browse/QTBUG-44895
    https://codereview.qt-project.org/#/c/111932/
    
    However this is no excusive for not fixing #70

--- src/libgreenisland/screen/nativescreenbackend.cpp
+++ src/libgreenisland/screen/nativescreenbackend.cpp
@@ -32,15 +32,6 @@
 
 Q_LOGGING_CATEGORY(NATIVE_BACKEND, "greenisland.screenbackend.native")
 
-// QtDeclarative 5.5 suffers from this bug: QTBUG-45279
-// which happens here on multi screen setups, as a temporary
-// workaround we only create the primary output
-#if QT_VERSION >= QT_VERSION_CHECK(5, 5, 0)
-#  define QTBUG_WORKAROUND 1
-#else
-#  define QTBUG_WORKAROUND 0
-#endif
-
 namespace GreenIsland {
 
 NativeScreenBackend::NativeScreenBackend(Compositor *compositor, QObject *parent)
@@ -62,22 +53,15 @@ void NativeScreenBackend::acquireConfiguration()
     disconnect(qGuiApp, &QGuiApplication::screenRemoved,
                this, &NativeScreenBackend::screenRemoved);
 
-    Q_FOREACH (QScreen *screen, qGuiApp->screens()) {
-#if QTBUG_WORKAROUND
-        if (qGuiApp->primaryScreen() != screen)
-            continue;
-#endif
+    Q_FOREACH (QScreen *screen, qGuiApp->screens())
         screenAdded(screen);
-    }
 
     Q_EMIT configurationAcquired();
 
-#if !QTBUG_WORKAROUND
     connect(qGuiApp, &QGuiApplication::screenAdded,
             this, &NativeScreenBackend::screenAdded);
     connect(qGuiApp, &QGuiApplication::screenRemoved,
             this, &NativeScreenBackend::screenRemoved);
-#endif
 }
 
 void NativeScreenBackend::screenAdded(QScreen *screen)
@@ -157,11 +141,7 @@ void NativeScreenBackend::changePosition(QScreen *screen)
         return;
 
     Output *output = m_screenMap[screen];
-#if QTBUG_WORKAROUND
-    output->setPosition(QPoint(0, 0));
-#else
     output->setPosition(screen->availableGeometry().topLeft());
-#endif
     output->window()->setPosition(output->position());
 }
 
