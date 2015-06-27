5762d0746cb48d3586e129424c0b7ddd91526714
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-06-27 08:14:25 +0200

    launcher: Improve the launcher

--- src/launcher/main.cpp
+++ src/launcher/main.cpp
@@ -1,17 +1,44 @@
 /****************************************************************************
- * This file is part of Green Island.
+ * This file is part of Hawaii.
  *
- * Copyright (C) 2014 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ * Copyright (C) 2014-2015 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
  *
  * Author(s):
  *    Pier Luigi Fiorini
  *
- * $BEGIN_LICENSE:GPL2+$
+ * $BEGIN_LICENSE:GPL3-HAWAII$
  *
- * This program is free software: you can redistribute it and/or modify
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation; version 3 or any later version accepted
+ * by Pier Luigi Fiorini, which shall act as a proxy defined in Section 14
+ * of version 3 of the license.
+ *
+ * Any modifications to this file must keep this entire header intact.
+ *
+ * The interactive user interfaces in modified source and object code
+ * versions of this program must display Appropriate Legal Notices,
+ * as required under Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Powered by Hawaii" logo.  If the display of the logo is not reasonably
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Powered by Hawaii".
+ *
+ * In accordance with Section 7(c) of the GNU General Public License
+ * version 3, modified source and object code versions of this program
+ * must be marked in reasonable ways as different from the original version.
+ *
+ * In accordance with Section 7(d) of the GNU General Public License
+ * version 3, neither the "Hawaii" name, nor the name of any project that is
+ * related to it, nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written
+ * permission.
+ *
+ * In accordance with Section 7(e) of the GNU General Public License
+ * version 3, this license does not grant any license or rights to use the
+ * "Hawaii" name or logo, nor any other trademark.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -29,7 +56,7 @@
 #include <QtCore/QLoggingCategory>
 
 #include "config.h"
-#include "processcontroller.h"
+#include "compositorlauncher.h"
 
 #define TR(x) QT_TRANSLATE_NOOP("Command line parser", QStringLiteral(x))
 
@@ -39,7 +66,8 @@ int main(int argc, char *argv[])
     QCoreApplication app(argc, argv);
     app.setApplicationName(QStringLiteral("Green Island"));
     app.setApplicationVersion(QStringLiteral(GREENISLAND_VERSION_STRING));
-    app.setOrganizationName(QStringLiteral("Green Island"));
+    app.setOrganizationName(QStringLiteral("Hawaii"));
+    app.setOrganizationDomain(QStringLiteral("hawaiios.org"));
 
     // Command line parser
     QCommandLineParser parser;
@@ -47,31 +75,51 @@ int main(int argc, char *argv[])
     parser.addHelpOption();
     parser.addVersionOption();
 
-    // Full screen shell
-    QCommandLineOption fullScreenShellOption(QStringLiteral("fullscreen-shell"),
-                                             TR("Nest Green Island into a full screen shell compositor"));
-    parser.addOption(fullScreenShellOption);
+    // Mode
+    QCommandLineOption modeOption(QStringList() << QStringLiteral("m") << QStringLiteral("mode"),
+                                  TR("Specify mode (eglfs, hwcomposer, nested, x11, wayland)."),
+                                  TR("mode"));
+    parser.addOption(modeOption);
 
-    // Compositor package
-    QCommandLineOption shellOption(QStringList() << QStringLiteral("s") << QStringLiteral("shell"),
-                                   TR("Force loading the given shell"),
-                                   QStringLiteral("shell"));
-    parser.addOption(shellOption);
+    // Applications to start
+    QCommandLineOption exeOption(QStringLiteral("execute"),
+                                 TR("Command to execute."), TR("command"));
+    parser.addOption(exeOption);
 
     // Parse command line
     parser.process(app);
 
-    // Process controller that manages the compositor
-    ProcessController processController;
-
-    // Force full screen shell if requested
-    processController.setFullScreenShellEnabled(parser.isSet(fullScreenShellOption));
-
-    // Pass additional arguments
-    processController.setShell(parser.value(shellOption));
+    // Assume greenisland by default
+    QString program = QStringLiteral("greenisland");
+    QStringList arguments;
+    if (parser.isSet(exeOption)) {
+        arguments = parser.value(exeOption).split(QLatin1Char(' '),
+                                                  QString::SkipEmptyParts);
+        program = arguments.takeFirst();
+    }
 
     // Start the compositor
-    processController.start();
+    CompositorLauncher launcher(program, arguments);
+    if (parser.isSet(modeOption)) {
+        QString modeString = parser.value(modeOption);
+        CompositorLauncher::Mode mode = CompositorLauncher::UnknownMode;
+        if (modeString == QStringLiteral("eglfs"))
+            mode = CompositorLauncher::EglFSMode;
+        else if (modeString == QStringLiteral("hwcomposer"))
+            mode = CompositorLauncher::HwComposerMode;
+        else if (modeString == QStringLiteral("nested"))
+            mode = CompositorLauncher::NestedMode;
+        else if (modeString == QStringLiteral("x11"))
+            mode = CompositorLauncher::X11Mode;
+        else if (modeString == QStringLiteral("wayland"))
+            mode = CompositorLauncher::WaylandMode;
+        else
+            qFatal("Invalid mode \"%s\" specified, accepted values are: "
+                   "eglfs, hwcomposer, nested, x11, wayland",
+                   qPrintable(modeString));
+        launcher.setMode(mode);
+    }
+    launcher.start();
 
     return app.exec();
 }
