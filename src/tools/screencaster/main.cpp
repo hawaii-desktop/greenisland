8945e5db79a10417ab59c1c1a621f4026e4e1554
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-08-26 21:47:59 +0200

    Update header for screen recording

--- src/tools/screencaster/main.cpp
+++ src/tools/screencaster/main.cpp
@@ -1,29 +1,22 @@
 /****************************************************************************
  * This file is part of Green Island.
  *
- * Copyright (C) 2015 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ * Copyright (C) 2015 Pier Luigi Fiorini
  * Copyright (C) 2014 Jolla Ltd.
  *
  * Author(s):
- *    Pier Luigi Fiorini
+ *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
  *
  * Original Author(s):
  *    Giulio Camuffo <giulio.camuffo@jollamobile.com>
  *
- * $BEGIN_LICENSE:LGPL2.1+$
+ * $BEGIN_LICENSE:LGPL2.1$
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation
+ * and appearing in the file LICENSE.LGPLv21 included in the packaging
+ * of this file.
  *
  * $END_LICENSE$
  ***************************************************************************/
