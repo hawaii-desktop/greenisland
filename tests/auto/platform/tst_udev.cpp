76ae89464e840afb445c08279a627d4584b72da0
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-09-05 15:30:26 +0200

    Test case for udev
    Issue: #47

---
+++ tests/auto/platform/tst_udev.cpp
@@ -0,0 +1,80 @@
+/****************************************************************************
+ * This file is part of Green Island.
+ *
+ * Copyright (C) 2015 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini
+ *
+ * $BEGIN_LICENSE:GPL2+$
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
+#include <QtTest/QtTest>
+
+#include <GreenIslandPlatform/Udev>
+#include <GreenIslandPlatform/UdevEnumerate>
+
+using namespace GreenIsland::Platform;
+
+class TestUdev : public QObject
+{
+    Q_OBJECT
+public:
+    TestUdev(QObject *parent = 0)
+        : QObject(parent)
+    {
+    }
+
+private Q_SLOTS:
+    void testConnection()
+    {
+        Udev *udev = new Udev;
+        QVERIFY(udev->isValid());
+        delete udev;
+    }
+
+    void testDevice()
+    {
+        Udev *udev = new Udev;
+        QVERIFY(udev->isValid());
+
+        UdevDevice *dev = udev->deviceFromFileName(QStringLiteral("/dev/sda"));
+        QCOMPARE(dev->deviceNode(), QStringLiteral("/dev/sda"));
+        QVERIFY(dev);
+
+        delete dev;
+        delete udev;
+    }
+
+    void testEnumerate()
+    {
+        Udev *udev = new Udev;
+        QVERIFY(udev->isValid());
+
+        UdevEnumerate *enumerate = new UdevEnumerate(UdevDevice::InputDevice_Mask, udev);
+        QList<UdevDevice *> devices = enumerate->scan();
+        QVERIFY(devices.size() > 0);
+
+        delete enumerate;
+        delete udev;
+    }
+};
+
+QTEST_MAIN(TestUdev)
+
+#include "tst_udev.moc"
