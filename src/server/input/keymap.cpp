cdf8a81ac4a6ab7ac67b8b12a20ddfd7d9f98a03
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-11-28 17:14:03 +0100

    server: Say hello to Keymap
    Add a class for keymaps.
    
    Issue: #141

---
+++ src/server/input/keymap.cpp
@@ -0,0 +1,128 @@
+/****************************************************************************
+ * This file is part of Green Island.
+ *
+ * Copyright (C) 2015 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini
+ *
+ * $BEGIN_LICENSE:LGPL2.1+$
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
+#include "keymap.h"
+#include "keymap_p.h"
+
+namespace GreenIsland {
+
+namespace Server {
+
+Keymap::Keymap(QObject *parent)
+    : QObject(*new KeymapPrivate(), parent)
+{
+}
+
+QString Keymap::layout() const
+{
+    Q_D(const Keymap);
+    return d->layout;
+}
+
+void Keymap::setLayout(const QString &layout)
+{
+    Q_D(Keymap);
+
+    if (d->layout == layout)
+        return;
+
+    d->layout = layout;
+    Q_EMIT layoutChanged();
+}
+
+QString Keymap::variant() const
+{
+    Q_D(const Keymap);
+    return d->variant;
+}
+
+void Keymap::setVariant(const QString &variant)
+{
+    Q_D(Keymap);
+
+    if (d->variant == variant)
+        return;
+
+    d->variant = variant;
+    Q_EMIT variantChanged();
+}
+
+QString Keymap::options() const
+{
+    Q_D(const Keymap);
+    return d->options;
+}
+
+void Keymap::setOptions(const QString &options)
+{
+    Q_D(Keymap);
+
+    if (d->options == options)
+        return;
+
+    d->options = options;
+    Q_EMIT optionsChanged();
+}
+
+QString Keymap::model() const
+{
+    Q_D(const Keymap);
+    return d->model;
+}
+
+void Keymap::setModel(const QString &model)
+{
+    Q_D(Keymap);
+
+    if (d->model == model)
+        return;
+
+    d->model = model;
+    Q_EMIT modelChanged();
+}
+
+QString Keymap::rules() const
+{
+    Q_D(const Keymap);
+    return d->rules;
+}
+
+void Keymap::setRules(const QString &rules)
+{
+    Q_D(Keymap);
+
+    if (d->rules == rules)
+        return;
+
+    d->rules = rules;
+    Q_EMIT rulesChanged();
+}
+
+} // namespace Server
+
+} // namespace GreenIsland
+
+#include "moc_keymap.cpp"
---
+++ src/server/input/keymap.h
@@ -0,0 +1,79 @@
+/****************************************************************************
+ * This file is part of Green Island.
+ *
+ * Copyright (C) 2015 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini
+ *
+ * $BEGIN_LICENSE:LGPL2.1+$
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
+#ifndef GREENISLAND_KEYMAP_H
+#define GREENISLAND_KEYMAP_H
+
+#include <QtCore/QObject>
+
+#include <GreenIsland/server/greenislandserver_export.h>
+
+namespace GreenIsland {
+
+namespace Server {
+
+class KeymapPrivate;
+
+class GREENISLANDSERVER_EXPORT Keymap : public QObject
+{
+    Q_OBJECT
+    Q_DECLARE_PRIVATE(Keymap)
+    Q_PROPERTY(QString layout READ layout WRITE setLayout NOTIFY layoutChanged)
+    Q_PROPERTY(QString variant READ variant WRITE setVariant NOTIFY variantChanged)
+    Q_PROPERTY(QString options READ options WRITE setOptions NOTIFY optionsChanged)
+    Q_PROPERTY(QString model READ model WRITE setModel NOTIFY modelChanged)
+    Q_PROPERTY(QString rules READ rules WRITE setRules NOTIFY rulesChanged)
+public:
+    Keymap(QObject *parent = Q_NULLPTR);
+
+    QString layout() const;
+    void setLayout(const QString &layout);
+
+    QString variant() const;
+    void setVariant(const QString &variant);
+
+    QString options() const;
+    void setOptions(const QString &options);
+
+    QString model() const;
+    void setModel(const QString &model);
+
+    QString rules() const;
+    void setRules(const QString &rules);
+
+Q_SIGNALS:
+    void layoutChanged();
+    void variantChanged();
+    void optionsChanged();
+    void modelChanged();
+    void rulesChanged();
+};
+
+} // namespace Server
+
+} // namespace GreenIsland
+
+#endif // GREENISLAND_KEYMAP_H
