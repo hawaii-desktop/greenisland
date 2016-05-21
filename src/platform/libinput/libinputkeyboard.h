591658a1ea120326b6e32996e2f70ccf3764e08b
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2016-05-21 11:20:48 +0200

    Update license header
    Changes made here are meant to be under LGPL v3 or GPL v2+ not the
    Qt commercial license.

--- src/platform/libinput/libinputkeyboard.h
+++ src/platform/libinput/libinputkeyboard.h
@@ -6,21 +6,26 @@
  * Author(s):
  *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
  *
- * $BEGIN_LICENSE:LGPL$
+ * $BEGIN_LICENSE:QTLGPL$
  *
- * This file may be used under the terms of the GNU Lesser General Public
- * License version 2.1 or later as published by the Free Software Foundation
- * and appearing in the file LICENSE.LGPLv21 included in the packaging of
- * this file.  Please review the following information to ensure the
- * GNU Lesser General Public License version 2.1 requirements will be
- * met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+ * GNU Lesser General Public License Usage
+ * This file may be used under the terms of the GNU Lesser General
+ * Public License version 3 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.LGPLv3 included in the
+ * packaging of this file. Please review the following information to
+ * ensure the GNU Lesser General Public License version 3 requirements
+ * will be met: https://www.gnu.org/licenses/lgpl.html.
  *
- * Alternatively, this file may be used under the terms of the GNU General
- * Public License version 2.0 or later as published by the Free Software
- * Foundation and appearing in the file LICENSE.GPLv2 included in the
- * packaging of this file.  Please review the following information to ensure
- * the GNU General Public License version 2.0 requirements will be
- * met: http://www.gnu.org/licenses/gpl-2.0.html.
+ * GNU General Public License Usage
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 2.0 or (at your option) the GNU General
+ * Public license version 3 or any later version approved by the KDE Free
+ * Qt Foundation. The licenses are as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPLv2 and LICENSE.GPLv3
+ * included in the packaging of this file. Please review the following
+ * information to ensure the GNU General Public License requirements will
+ * be met: https://www.gnu.org/licenses/gpl-2.0.html and
+ * https://www.gnu.org/licenses/gpl-3.0.html.
  *
  * $END_LICENSE$
  ***************************************************************************/
