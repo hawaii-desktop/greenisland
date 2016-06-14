46b22ab37100da7a48f471ca7fba37dc9dba8f0e
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2016-06-14 19:42:42 +0200

    Fixed invalid texture error when unhiding QWaylandQuickItems.
    When creating new scene-graph nodes (which is the case when re-parenting
    items), no texture is attached to the node, leading to an abort due to an
    invalid scene-graph.
    
    Done-with: Robert Griebl<robert.griebl@pelagicore.com>

--- src/waylandcompositor/compositor_api/qwaylandquickitem.cpp
+++ src/waylandcompositor/compositor_api/qwaylandquickitem.cpp
@@ -1123,8 +1123,10 @@ QSGNode *QWaylandQuickItem::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeDat
     if (ref.isShm() || bufferTypes[ref.bufferFormatEgl()].canProvideTexture) {
         QSGSimpleTextureNode *node = static_cast<QSGSimpleTextureNode *>(oldNode);
 
-        if (!node)
+        if (!node) {
             node = new QSGSimpleTextureNode();
+            d->newTexture = true;
+        }
 
         if (!d->provider)
             d->provider = new QWaylandSurfaceTextureProvider();
@@ -1144,8 +1146,10 @@ QSGNode *QWaylandQuickItem::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeDat
 
         QSGGeometryNode *node = static_cast<QSGGeometryNode *>(oldNode);
 
-        if (!node)
+        if (!node) {
             node = new QSGGeometryNode;
+            d->newTexture = true;
+        }
 
         QSGGeometry *geometry = node->geometry();
         QWaylandBufferMaterial *material = static_cast<QWaylandBufferMaterial *>(node->material());
