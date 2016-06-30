52b8b24a80448025805defab3e683f04cb7acd3a
Pier Luigi Fiorini <pierluigi.fiorini@hawaiios.org>
2016-06-30 21:28:02 +0200

    Update headers

--- src/client/shmpool.h
+++ src/client/shmpool.h
@@ -2,25 +2,28 @@
  * This file is part of Hawaii.
  *
  * Copyright (C) 2015-2016 Pier Luigi Fiorini
+ * Copyright (C) 2013 Martin Gräßlin
  *
  * Author(s):
  *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *    Martin Gräßlin <mgraesslin@kde.org>
  *
- * $BEGIN_LICENSE:LGPL$
+ * $BEGIN_LICENSE:LGPLKDE$
  *
- * This file may be used under the terms of the GNU Lesser General Public
- * License version 2.1 or later as published by the Free Software Foundation
- * and appearing in the file LICENSE.LGPLv21 included in the packaging of
- * this file.  Please review the following information to ensure the
- * GNU Lesser General Public License version 2.1 requirements will be
- * met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) version 3, or any later version accepted by the membership
+ * of KDE e.V. (or its successor approved by the membership of KDE e.V.),
+ * which shall act as a proxy defined in Section 6 of version 3 of the license.
  *
- * Alternatively, this file may be used under the terms of the GNU General
- * Public License version 2.0 or later as published by the Free Software
- * Foundation and appearing in the file LICENSE.GPLv2 included in the
- * packaging of this file.  Please review the following information to ensure
- * the GNU General Public License version 2.0 requirements will be
- * met: http://www.gnu.org/licenses/gpl-2.0.html.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * $END_LICENSE$
  ***************************************************************************/
